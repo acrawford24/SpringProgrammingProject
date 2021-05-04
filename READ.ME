f09963a32fe528f9e68195c42bb35bd92027c3eb
abbiecrawford <>
2021-05-04 13:14:56 +0000

    Initial commit

---
+++ READ.ME
@@ -0,0 +1,41 @@
+[![Work in Repl.it](https://classroom.github.com/assets/work-in-replit-14baed9a392b3a25080506f3b7b6d57f295ec2978f6f33ec97e36a161684cbe9.svg)](https://classroom.github.com/online_ide?assignment_repo_id=410719&assignment_repo_type=GroupAssignmentRepo)
+
+# Project Title
+Vacation Planner
+
+## Description
+A program that uses user input to plan a vaction. Through this you will get a destination, hotel, and an activity. By the end of the program you will have a full vacation planned!
+
+## Getting Started 
+ 
+### Dependencies 
+* The only thing you should need in order to fork this repl is a repl.it account, if you just want to run the program to plan a vacation you should be able to wihtout an account.
+### Installing
+* You can fork this program on Repl.it
+
+### Executing Program 
+
+## Help 
+There should not be any bugs or issues in this program. If there is please use the contact info provided below. 
+
+## Authors 
+* Payton Curall - pcurall24@biomedscienceacademy.org
+* Cara Tiller - ctiller24@biomedscienceacademy.org 
+* Abbie Crawford - acrawford24@biomedscienceacademy.org
+
+## Version History 
+* 0.1
+    * Initial Release
+
+
+## License 
+This project is not licensed.
+
+## Acknowledgements
+
+
+* [geolocation-python 0.2.2] (https://pypi.org/project/geolocation-python/)
+* [Python Lists] (https://www.geeksforgeeks.org/python-list/)
+* [Trip Advisor] (https://www.tripadvisor.com/)
+*  [Python Dictionaries]
+(https://www.w3schools.com/python/python_dictionaries.asp)
---
+++ main.py
@@ -0,0 +1,98 @@
+weather_list = ["Sunny", "Snowing", "Rainy", "Tropical"]
+sunny_location_list = ["Orlando, Florida", "Austin, Texas","Los Angeles, California"]
+sunny_hotel_list = ["Crowne Plaza Orlando", "Hyatt Regency Orlando", "Hilton Orlando","JW Marriott Austin", "The Westin Austin Downtown", "Hilton Garden Inn Cedar Park Austin","Millennium Biltmore Hotel Los Angeles", "The Wayfarer Downtown Los Angeles", "Freehand Los Angles"]
+sunny_activity_list = ["Walt Disney World","Gatorland","Wonderworks","McKinney Falls", "State Park","Austin Zoo", "Cathedral of Junk","Universal","Studios-Hollywood","Dodgers Game", 
+"Hollywood Sign Hike"]
+
+snowing_location_list = ["Juneau, Alaska", "Boulder, Colorado", "Cleveland, Ohio"]
+snowing_hotel_list = ["Four Points by Sheraton Juneau", "Silverbow Inn Hotel & Suites", "Aspen Suites Hotel Juneau", "Hyatt House Seattle/Downtown", "Warwick Seattle" , "The Paramount Hotel","The Westin Downtown Cleveland", "Metropolitan at the 9", "Rennisance Cleveland Hotel"]
+snowing_activity_list = ["Patsy Ann Statue", "Goldbelt Tram ", "Alaska Adventure Concierge", "Flagstaff Mountain", "Flatirons", "Royal Arch Trail", "Rock and Roll Hall of Fame", "West Side Market", "A Christmas Story House"]
+
+rainy_location_list = ["Seattle, Washington","New Orleans, Lousianna",
+"Pittsburgh, Pennsylvania"]
+rainy_hotel_list = ["Hyatt House Seattle/Downtown", "Warwick Seattle"," The Paramount Hotel","St. James Hotel New Orleans Downtown", "Embassy Suites by Hilton New Orleans", "Hampton Inn & Suites New Orleans Downtown","DoubleTree by Hilton Hotel & Suites Pittsburgh Downtown", "Fairmont Pittsburgh", "Kimpton Hotel Monaco Pittsburgh"]
+rainy_activity_list = ["Pike Place Market","Space Needle","The Gum Wall","The National World War II Museum","Mardi Gras World","Bourbon Street","Point State Park","Carnegie Science Center","The Andy Warhol Museum"]
+
+tropical_location_list = ["Honolulu, Hawaii", "Puerto Rico", "Key West, Florida"]
+tropical_hotel_list = ["Hilton Hawaiian Village Waikiki Beach Resort", "Prince Waikiki - Honolulu Luxury Hotel", "Hotel Renew", "The St. Regis Bahia Beach Resort, Puerto Rico", "San Juan Water and Beach Club Hotel", "Dorado Beach, a Ritz-Carlton Reserve", "DoubleTree Resort by Hilton Hotel Grand Key - Key West", "Courtyard by Marriott Key West Waterfront", "Hyatt Residence Club Key West, Windward Pointe"]
+tropical_acticity_list = ["Diamond Head", "Pearl Harbor National Memorial", "Manoa Falls", "Bahía Bioluminiscente", "Castillo San Felipe del Morro", "La Fortaleza", "Southernmost Point of the Continental US", "Mallory Square", "Dog Beach"]
+
+vacation = input("Do you want to take a vacation? ")
+while vacation == "no":
+  vacation = input("Do you want to take a vacation? ")
+if vacation == "yes":
+
+  print(weather_list) 
+  weather_choices = input("Out of the choices above, what weather would you prefer for your vacation? ")
+
+if weather_choices == weather_list[0]:
+  print(sunny_location_list)
+  location_question = input("Out of the list above, which location would you prefer? ")
+  if location_question == sunny_location_list[0]:
+    print("\n"+sunny_hotel_list[0] + ",\n" + sunny_hotel_list[1] + ",\n" + sunny_hotel_list[2])
+    hotel_question = input("Out of the list above, which hotel would you prefer? ")
+    if hotel_question in sunny_hotel_list:
+      print("\n"+sunny_activity_list[0] + ",\n" + sunny_activity_list[1] + ",\n" + sunny_activity_list[2])
+      activity_question = input("Out of the list above, which activity would you prefer? ")
+      final_trip = [weather_choices, location_question, hotel_question, activity_question]
+      print(final_trip)
+  elif location_question == sunny_location_list[1]:
+    print("\n"+sunny_hotel_list[3] + ",\n" + sunny_hotel_list[4] + ",\n" + sunny_hotel_list[5])
+    hotel_question = input("Out of the list above, which hotel would you prefer? ")
+    if hotel_question in sunny_hotel_list:
+      print("\n"+sunny_activity_list[3] + ",\n" + sunny_activity_list[4] + ",\n" + sunny_activity_list[5])
+      activity_question = input("Out of the list above, which activity would you prefer? ")
+      final_trip = [weather_choices, location_question, hotel_question, activity_question]
+      print(final_trip)
+  elif location_question == sunny_location_list[1]:
+    print("\n"+sunny_hotel_list[3] + ",\n" + sunny_hotel_list[4] + ",\n" + sunny_hotel_list[5])
+    hotel_question = input("Out of the list above, which hotel would you prefer? ")
+    if hotel_question == sunny_hotel_list[3] or sunny_hotel_list[4] or sunny_hotel_list[5]:
+      print(sunny_activity_list[3] + ",\n" + sunny_activity_list[4] + ",\n" + sunny_activity_list[5])
+      activity_question = input("Out of the list above, which activity would you prefer? ")
+      final_trip = [weather_choices, location_question, hotel_question, activity_question]
+      print(final_trip)
+  elif location_question == sunny_location_list[2]:
+    print(sunny_hotel_list[6] + ",\n" + sunny_hotel_list[7] + ",\n" + sunny_hotel_list[8])
+    hotel_question = input("Out of the list above, which hotel would you prefer? ")
+    if hotel_question == sunny_hotel_list[6] or sunny_hotel_list[7] or sunny_hotel_list[8]:
+      print(sunny_activity_list[6] + ",\n" + sunny_activity_list[7] + ",\n" + sunny_activity_list[8])
+      activity_question = input("Out of the list above, which activity would you prefer? ")
+      final_trip = [weather_choices, location_question, hotel_question, activity_question]
+      print(final_trip)
+      
+elif weather_choices == weather_list[1]:
+  print(snowing_location_list)
+  location_question = input("Out of the list above, which location would you prefer? ")
+  if location_question == snowing_location_list[0]:
+    print("\n"+snowing_hotel_list[0] + ",\n" + snowing_hotel_list[1] + ",\n" + snowing_hotel_list[2])
+    hotel_question = input("Out of the list above, which hotel would you prefer? ")
+    if hotel_question == snowing_hotel_list[0] or snowing_hotel_list[1] or snowing_hotel_list[2]:
+      print("\n"+snowing_activity_list[0] + ",\n " + snowing_activity_list[1] + ",\n" + snowing_activity_list[2])
+      activity_question = input("Out of the list above, which activity would you prefer? ")
+      final_trip = [weather_choices, location_question, hotel_question, activity_question]
+      print(final_trip)
+  elif location_question == snowing_location_list[1]:
+    print(snowing_hotel_list[3] + ", " + snowing_hotel_list[4] + ", " + snowing_hotel_list[5])
+    hotel_question = input("Out of the list above, which hotel would you prefer? ")
+    if hotel_question == snowing_hotel_list[3] or snowing_hotel_list[4] or snowing_hotel_list[5]:
+      print(snowing_activity_list[3] + ", " + snowing_activity_list[4] + ", " + snowing_activity_list[5])
+      activity_question = input("Out of the list above, which activity would you prefer? ")
+      final_trip = [weather_choices, location_question, hotel_question, activity_question]
+      print(final_trip)
+  elif location_question == snowing_location_list[1]:
+    print(snowing_hotel_list[3] + ", " + snowing_hotel_list[4] + ", " + snowing_hotel_list[5])
+    hotel_question = input("Out of the list above, which hotel would you prefer? ")
+    if hotel_question == snowing_hotel_list[3] or snowing_hotel_list[4] or snowing_hotel_list[5]:
+      print(snowing_activity_list[3] + ", " + snowing_activity_list[4] + ", " + snowing_activity_list[5])
+      activity_question = input("Out of the list above, which activity would you prefer? ")
+      final_trip = [weather_choices, location_question, hotel_question, activity_question]
+      print(final_trip)
+  elif location_question == snowing_location_list[2]:
+    print(snowing_hotel_list[6] + ", " + snowing_hotel_list[7] + ", " + snowing_hotel_list[8])
+    hotel_question = input("Out of the list above, which hotel would you prefer? ")
+    if hotel_question == snowing_hotel_list[6] or snowing_hotel_list[7] or snowing_hotel_list[8]:
+      print(snowing_activity_list[6] + ", " + snowing_activity_list[7] + ", " + snowing_activity_list[8])
+      activity_question = input("Out of the list above, which activity would you prefer? ")
+      final_trip = [weather_choices, location_question, hotel_question, activity_question]
+      print(final_trip)
